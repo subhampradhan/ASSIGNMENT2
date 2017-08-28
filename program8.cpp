7005a7e9d69284ed3e6777296740fd1835b954a4
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 23:01:27 +0530

    Create program8.cpp

---
+++ program8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b,c;
+	cin>>a>>b;
+	c=0.5*a*b;
+	cout<<"base of tringle is "<<a<<" height of tringle is "<<b<<" then area="<<c<<endl;
+	return 0;
+}
