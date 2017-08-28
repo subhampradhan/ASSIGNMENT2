094fa3b76fa5e99a88a303ee3e6cd61fad5797c7
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 17:26:40 +0530

    Create program5.cpp

---
+++ program5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{ 
+  int a,b,p;
+  cin>>a>>b;
+  p=pow(a,b);
+  cout<<"base="<<a<<" exp="<<b<<" the result base raised by power exp="<<p<<endl;
+  return 0;
+}
