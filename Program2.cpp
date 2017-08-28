e9b96c11a204185a3171560cd2fbcea449907193
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 15:39:46 +0530

    Create Program2.cpp

---
+++ Program2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b;
+	cin>>a;
+	cout<<"temp in celcius="<<a<<endl;
+	b=1.8*a+32;
+	cout<<"temp in fahrenheit="<<b<<endl;
+	return 0;
+}
