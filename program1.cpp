a7e06b761b5e106400302a7a9c85b53328870add
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-29 00:05:58 +0530

    Create program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b,c;
+	cin>>a;
+	b=a/100;
+	c=a/100000;
+	cout<<"length in cm="<<a<<" length in meter="<<b<<" length in kilometer="<<c<<endl;
+	return 0;
+}
