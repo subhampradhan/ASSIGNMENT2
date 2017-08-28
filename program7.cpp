cfd779cf05dc9e240e740b5c80c5bac1d8c614e5
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 22:43:27 +0530

    Create program7.cpp

---
+++ program7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin>>a>>b;
+	c=180-(a+b);
+	cout<<"two angles of tringle are "<<a<<" & "<<b<<" the other angle is "<<c<<endl;
+	return 0;
+}
