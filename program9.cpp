1a307be209e51592a3052a1d329012eda0dd2cae
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 23:23:10 +0530

    Create program9.cpp

---
+++ program9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	float a,c;
+	cin>>a;
+	c=a*a*(sqrt(3))/4;
+	cout<<"side of equilateral tringle is "<<a<<" then area="<<c<<endl;
+	return 0;
+}
